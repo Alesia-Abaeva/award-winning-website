d1115ececcd5cfa69c0890336e9ed2f80d159635
Alesia <alesia.litvinenko@gmail.com>
2025-08-10 12:57:35 +0300

    Init project and add normalyzing styles

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-orange-50 bg-amber-950">
+      App
+      <h1 className="font-zentry">font-zentry</h1>
+    </div>
+  )
+}
+
+export default App
